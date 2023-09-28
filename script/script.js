2a55b204fe11251ad9a1053f7e0a920f4bc7c88c
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-09-28 10:46:05 -0300

    realizando el query 480px

--- script/script.js
+++ script/script.js
@@ -1,11 +1,1 @@
 /*---------------------menu-hamburguesa para celular-------------------------*/
-
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".menu-icon");
-    const menuList = document.querySelector(".menu");
-  
-    menuIcon.addEventListener("click", function () {
-      menuList.classList.toggle("show-menu");
-    });
-  });
-  
