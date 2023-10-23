f32fc689cc994f2d067d5aebf3ca85a1dc57bf72
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-10-22 23:44:33 -0300

    iniciando botones seccion item js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-/*---------------------menu-hamburguesa para celular-------------------------*/
+/*---------------------Seccion-item-------------------------*/
+ /*---------------------Botones------------------*/
+ const add=document.querySelector('#add');
+ const subtract=document.querySelector('#subtract');
+ const quantity=document.querySelector('#quantity');
+
+ console.log(add, subtract, quantity);
