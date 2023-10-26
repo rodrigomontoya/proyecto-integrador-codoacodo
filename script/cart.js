fbb6be563a58c2d54e0922f8aca3bf9896561547
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-10-26 13:40:20 -0300

    terminando js de cart falta  la fun de eli item

--- script/cart.js
+++ script/cart.js
@@ -1,50 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtén los botones de aumento y disminución
-    const incrementButtons = document.querySelectorAll('.producto-cantidad button:nth-child(1)');
-    const decrementButtons = document.querySelectorAll('.producto-cantidad button:nth-child(2)');
-    const cantidadInputs = document.querySelectorAll('.producto-cantidad input');
-    const precioUnitarioElements = document.querySelectorAll('.precioUnitario');
-    const productoTotalElements = document.querySelectorAll('.producto-total span');
-    const subtotalElement = document.querySelector('.subtotal-fila-2 .desc-numero');
-    const totalElement = document.querySelector('.total-fila-4 span');
-    
-    let subtotal = 0;
-    
-    // Función para actualizar el subtotal y el total
-    function updateTotals() {
-      subtotal = 0;
-      cantidadInputs.forEach((input, index) => {
-        const cantidad = parseInt(input.value);
-        const precioUnitario = parseFloat(precioUnitarioElements[index].textContent);
-        const total = cantidad * precioUnitario;
-        productoTotalElements[index].textContent = total.toFixed(2);
-        subtotal += total;
-      });
-      subtotal = parseFloat(subtotal); // Convertir a número
+  const cantidadInputs = document.querySelectorAll('.producto-cantidad input');
+  const precioUnitarioElements = document.querySelectorAll('.precioUnitario');
+  const productoTotalElements = document.querySelectorAll('.producto-total span');
+  const subtotalElement = document.querySelector('.subtotal-fila-2 .desc-numero');
+  const totalElement = document.querySelector('.total-fila-4 span');
+  const deleteButtons = document.querySelectorAll('.producto-eliminar img');
 
-      subtotalElement.textContent = '$' + subtotal.toFixed(2);
-      totalElement.textContent = '$' + subtotal.toFixed(2); // Mostrar el total como número con dos decimales
-    }
-    
-    // Agregar evento de clic para botones de aumento
-    incrementButtons.forEach((button, index) => {
-      button.addEventListener('click', function () {
-        let cantidad = parseInt(cantidadInputs[index].value);
-        cantidad++;
-        cantidadInputs[index].value = cantidad;
-        updateTotals();
-      });
+  function updateTotals() {
+    let subtotal = 0;
+    let itemCount = 0;
+
+    cantidadInputs.forEach((input, index) => {
+      const cantidad = parseInt(input.value) || 0;
+      const precioUnitario = parseFloat(precioUnitarioElements[index].textContent);
+      const total = cantidad * precioUnitario;
+      productoTotalElements[index].textContent = '$' + total.toFixed(2);
+      subtotal += total;
+      itemCount += cantidad;
     });
-    
-    // Agregar evento de clic para botones de disminución
-    decrementButtons.forEach((button, index) => {
-      button.addEventListener('click', function () {
-        let cantidad = parseInt(cantidadInputs[index].value);
-        if (cantidad > 0) {
-          cantidad--;
-          cantidadInputs[index].value = cantidad;
-          updateTotals();
-        }
-      });
+
+    subtotalElement.textContent = '$' + subtotal.toFixed(2);
+    totalElement.textContent = '$' + subtotal.toFixed(2);
+
+    document.querySelector('.cant-fila-1 .desc-numero').textContent = itemCount;
+  }
+
+  const incrementButtons = document.querySelectorAll('.producto-cantidad button:nth-child(1)');
+  const decrementButtons = document.querySelectorAll('.producto-cantidad button:nth-child(2)');
+
+  incrementButtons.forEach((button, index) => {
+    button.addEventListener('click', function () {
+      let cantidad = parseInt(cantidadInputs[index].value) || 0;
+      cantidad++;
+      cantidadInputs[index].value = cantidad;
+      updateTotals();
     });
   });
+
+  decrementButtons.forEach((button, index) => {
+    button.addEventListener('click', function () {
+      let cantidad = parseInt(cantidadInputs[index].value) || 0;
+      if (cantidad > 0) {
+        cantidad--;
+        cantidadInputs[index].value = cantidad;
+        updateTotals();
+      }
+    });
+  });
+
+  // Agregar evento para eliminar productos
+  deleteButtons.forEach((button, index) => {
+    button.addEventListener('click', function () {
+      // Encuentra el elemento padre del botón (div) y elimina ese elemento
+      const cartItem = button.closest('.cart-prod-fila');
+      cartItem.remove();
+      updateTotals();
+    });
+  });
+
+  // Llama a updateTotals después de cargar la página
+  updateTotals();
+});
+
+
