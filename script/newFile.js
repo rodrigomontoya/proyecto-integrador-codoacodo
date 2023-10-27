4ebd81249721cdd7d0158853b8cbb4316d564040
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-10-27 10:49:18 -0300

    funcionalidad js cart terminado

--- script/newFile.js
+++ script/newFile.js
@@ -5,9 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const subtotalElement = document.querySelector('.subtotal-fila-2 .desc-numero');
   const totalElement = document.querySelector('.total-fila-4 span');
   const deleteButtons = document.querySelectorAll('.producto-eliminar img');
-  const resumenSubtotalElement = document.querySelector('.subtotal-fila-2 .desc-numero');
-  const resumenTotalElement = document.querySelector('.total-fila-4 span');
-  const resumenCantidadElement = document.querySelector('.cant-fila-1 .desc-numero');
 
   function updateTotals() {
     let subtotal = 0;
@@ -25,10 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
     subtotalElement.textContent = '$' + subtotal.toFixed(2);
     totalElement.textContent = '$' + subtotal.toFixed(2);
 
-    resumenSubtotalElement.textContent = '$' + subtotal.toFixed(2);
-    resumenTotalElement.textContent = '$' + subtotal.toFixed(2);
-
-    resumenCantidadElement.textContent = itemCount;
+    document.querySelector('.cant-fila-1 .desc-numero').textContent = itemCount;
   }
 
   const incrementButtons = document.querySelectorAll('.producto-cantidad button:nth-child(1)');
@@ -59,40 +53,10 @@ document.addEventListener('DOMContentLoaded', function () {
     button.addEventListener('click', function () {
       // Encuentra el elemento padre del botón (div) y elimina ese elemento
       const cartItem = button.closest('.cart-prod-fila');
-      cartItem.remove();
       updateTotals();
-      updateResumenCantidades();
     });
   });
-
-  // Función para actualizar las cantidades y los totales en el resumen
-  function updateResumenCantidades() {
-    // Obtener todas las cantidades de productos en el carrito
-    const cantidadInputs = document.querySelectorAll('.producto-cantidad input');
-
-    // Calcular la cantidad total de elementos en el carrito
-    let itemCount = 0;
-    let subtotal = 0;
-
-    cantidadInputs.forEach((input, index) => {
-      const cantidad = parseInt(input.value) || 0;
-      const precioUnitario = parseFloat(precioUnitarioElements[index].textContent);
-      subtotal += cantidad * precioUnitario;
-      itemCount += cantidad;
-    });
-
-    // Actualiza las cantidades, el subtotal y el total en el resumen
-    resumenCantidadElement.textContent = itemCount;
-    resumenSubtotalElement.textContent = '$' + subtotal.toFixed(2);
-    resumenTotalElement.textContent = '$' + subtotal.toFixed(2);
-  }
 
   // Llama a updateTotals después de cargar la página
   updateTotals();
 });
-
-
-
-
-
-
